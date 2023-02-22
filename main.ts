a9b4b10b3768aac9a21f0ba70b78e592d4e31d84
huangshixuan1 <98266496+huangshixuan1@users.noreply.github.com>
2023-02-22 15:01:37 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,45 @@
 KSB045.onBtnChanged(KSB045.btnName.C, KSB045.pushType.down, function () {
-	
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.pause(50)
+    basic.clearScreen()
+})
+KSB045.onBtnChanged(KSB045.btnName.D, KSB045.pushType.down, function () {
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        . . . . #
+        . . . # .
+        . . # . .
+        `)
+    basic.pause(50)
+    basic.clearScreen()
+})
+KSB045.onBtnChanged(KSB045.btnName.F, KSB045.pushType.down, function () {
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # . . . .
+        . # . . .
+        . . # . .
+        `)
+    basic.pause(50)
+    basic.clearScreen()
 })
 KSB045.onBtnChanged(KSB045.btnName.E, KSB045.pushType.down, function () {
-	
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(50)
+    basic.clearScreen()
 })
 radio.setGroup(173)
-basic.forever(function () {
-	
-})
