c1e95d28ed2c4592dedb36451c59cd511e3a45a9
huangshixuan1 <98266496+huangshixuan1@users.noreply.github.com>
2023-02-22 13:06:36 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+KSB045.onBtnChanged(KSB045.btnName.C, KSB045.pushType.down, function () {
+	
+})
+KSB045.onBtnChanged(KSB045.btnName.E, KSB045.pushType.down, function () {
+	
+})
+radio.setGroup(173)
 basic.forever(function () {
 	
 })
