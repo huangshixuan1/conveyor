4a796ede8636c3a43a302009e4583a0ff6f3a2bc
huangshixuan1 <98266496+huangshixuan1@users.noreply.github.com>
2023-02-24 13:04:04 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,45 +1,46 @@
-KSB045.onBtnChanged(KSB045.btnName.C, KSB045.pushType.down, function () {
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        # . . . #
-        . # . # .
-        . . # . .
-        `)
-    basic.pause(50)
-    basic.clearScreen()
+function 向右轉 () {
+	
+}
+radio.onReceivedNumber(function (receivedNumber) {
+    // 1 x
+    // 3 x
+    // 7 向左轉
+    // 9 向右轉
+    // 8 上升
+    // 2 下降
+    // 4 向左跑
+    // 6 向右跑
+    if (receivedNumber == 1) {
+    	
+    } else if (receivedNumber == 3) {
+    	
+    } else if (receivedNumber == 7) {
+        向左轉()
+    } else if (receivedNumber == 9) {
+        向右轉()
+    } else if (receivedNumber == 8) {
+        上升()
+    } else if (receivedNumber == 2) {
+        下降()
+    } else if (receivedNumber == 4) {
+        向左跑()
+    } else if (receivedNumber == 6) {
+        向右跑()
+    }
 })
-KSB045.onBtnChanged(KSB045.btnName.D, KSB045.pushType.down, function () {
-    basic.showLeds(`
-        . . # . .
-        . . . # .
-        . . . . #
-        . . . # .
-        . . # . .
-        `)
-    basic.pause(50)
-    basic.clearScreen()
-})
-KSB045.onBtnChanged(KSB045.btnName.F, KSB045.pushType.down, function () {
-    basic.showLeds(`
-        . . # . .
-        . # . . .
-        # . . . .
-        . # . . .
-        . . # . .
-        `)
-    basic.pause(50)
-    basic.clearScreen()
-})
-KSB045.onBtnChanged(KSB045.btnName.E, KSB045.pushType.down, function () {
-    basic.showLeds(`
-        . . # . .
-        . # . # .
-        # . . . #
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(50)
-    basic.clearScreen()
-})
+function 向左轉 () {
+	
+}
+function 向左跑 () {
+	
+}
+function 上升 () {
+	
+}
+function 向右跑 () {
+	
+}
+function 下降 () {
+	
+}
 radio.setGroup(173)
