7d0a2d641d0efcf7b445eb3ff5f31beeb11532f1
huangshixuan1 <98266496+huangshixuan1@users.noreply.github.com>
2023-02-25 08:32:15 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,10 @@
-function 向右轉 () {
-    pwm = 0
-}
 radio.onReceivedNumber(function (receivedNumber) {
     // 1 x
     // 3 x
-    // 7 向左轉
-    // 9 向右轉
-    // 8 上升
-    // 2 下降
+    // 7 軌道向左轉
+    // 9 軌道向右轉
+    // 8 軌道上升
+    // 2 軌道下降
     // 4 向左跑
     // 6 向右跑
     if (receivedNumber == 1) {
@@ -15,33 +12,72 @@ radio.onReceivedNumber(function (receivedNumber) {
     } else if (receivedNumber == 3) {
     	
     } else if (receivedNumber == 7) {
-        向左轉()
+        軌道向左轉()
     } else if (receivedNumber == 9) {
-        向右轉()
+        軌道向右轉()
     } else if (receivedNumber == 8) {
-        上升()
+        軌道上升()
     } else if (receivedNumber == 2) {
-        下降()
+        軌道下降()
     } else if (receivedNumber == 4) {
         向左跑()
     } else if (receivedNumber == 6) {
         向右跑()
     }
 })
-function 向左轉 () {
-    pwm = 0
+function 軌道上升 () {
+    pwm = 255
+    sensors.DDMmotor(
+    AnalogPin.P0,
+    0,
+    AnalogPin.P0,
+    0
+    )
+}
+function 軌道下降 () {
+    pwm = 255
+    sensors.DDMmotor(
+    AnalogPin.P0,
+    0,
+    AnalogPin.P0,
+    0
+    )
 }
 function 向左跑 () {
-    pwm = 0
+    pwm = 255
+    sensors.DDMmotor(
+    AnalogPin.P0,
+    0,
+    AnalogPin.P0,
+    0
+    )
 }
-function 上升 () {
-    pwm = 0
+function 軌道向左轉 () {
+    pwm = 255
+    sensors.DDMmotor(
+    AnalogPin.P13,
+    0,
+    AnalogPin.P14,
+    pwm - 155
+    )
 }
 function 向右跑 () {
-    pwm = 0
+    pwm = 255
+    sensors.DDMmotor(
+    AnalogPin.P0,
+    0,
+    AnalogPin.P0,
+    0
+    )
 }
-function 下降 () {
-    pwm = 0
+function 軌道向右轉 () {
+    pwm = 255
+    sensors.DDMmotor(
+    AnalogPin.P13,
+    1,
+    AnalogPin.P14,
+    pwm - 155
+    )
 }
 let pwm = 0
 radio.setGroup(173)
