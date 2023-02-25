df951e78b0370683936c2558715ce22c745c4ef3
huangshixuan1 <98266496+huangshixuan1@users.noreply.github.com>
2023-02-25 09:12:47 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -12,69 +12,69 @@ radio.onReceivedNumber(function (receivedNumber) {
     } else if (receivedNumber == 3) {
     	
     } else if (receivedNumber == 7) {
-        軌道向左轉()
+        軌道向左轉7()
     } else if (receivedNumber == 9) {
-        軌道向右轉()
+        軌道向右轉9()
     } else if (receivedNumber == 8) {
-        軌道上升()
+        軌道上升8()
     } else if (receivedNumber == 2) {
-        軌道下降()
+        軌道下降2()
     } else if (receivedNumber == 4) {
-        向左跑()
+        向左跑4()
     } else if (receivedNumber == 6) {
-        向右跑()
+        向右跑6()
     }
 })
-function 軌道上升 () {
-    pwm = 255
-    sensors.DDMmotor(
-    AnalogPin.P0,
-    0,
-    AnalogPin.P0,
-    0
-    )
-}
-function 軌道下降 () {
-    pwm = 255
-    sensors.DDMmotor(
-    AnalogPin.P0,
-    0,
-    AnalogPin.P0,
-    0
-    )
-}
-function 向左跑 () {
-    pwm = 255
-    sensors.DDMmotor(
-    AnalogPin.P0,
-    0,
-    AnalogPin.P0,
-    0
-    )
-}
-function 軌道向左轉 () {
-    pwm = 255
-    sensors.DDMmotor(
-    AnalogPin.P13,
-    0,
-    AnalogPin.P14,
-    pwm - 155
-    )
-}
-function 向右跑 () {
-    pwm = 255
-    sensors.DDMmotor(
-    AnalogPin.P0,
-    0,
-    AnalogPin.P0,
-    0
-    )
-}
-function 軌道向右轉 () {
+function 軌道向右轉9 () {
     pwm = 255
     sensors.DDMmotor(
     AnalogPin.P13,
     1,
+    AnalogPin.P14,
+    pwm - 155
+    )
+}
+function 軌道下降2 () {
+    pwm = 255
+    sensors.DDMmotor(
+    AnalogPin.P0,
+    0,
+    AnalogPin.P0,
+    0
+    )
+}
+function 向左跑4 () {
+    pwm = 255
+    sensors.DDMmotor(
+    AnalogPin.P0,
+    0,
+    AnalogPin.P0,
+    0
+    )
+}
+function 向右跑6 () {
+    pwm = 255
+    sensors.DDMmotor(
+    AnalogPin.P0,
+    0,
+    AnalogPin.P0,
+    0
+    )
+}
+function 軌道上升8 () {
+    pwm = 255
+    sensors.DDMmotor(
+    AnalogPin.P0,
+    0,
+    AnalogPin.P0,
+    0
+    )
+}
+function 軌道向左轉7 () {
+    pwm = 255
+    sensors.DDMmotor(
+    AnalogPin.P13,
+    0,
     AnalogPin.P14,
     pwm - 155
     )
