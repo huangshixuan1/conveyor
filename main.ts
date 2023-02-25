84ef65cf44dca89e25f89e6577a78664b14b1598
huangshixuan1 <98266496+huangshixuan1@users.noreply.github.com>
2023-02-25 10:43:41 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -12,26 +12,36 @@ radio.onReceivedNumber(function (receivedNumber) {
     // 6 向右跑
     if (receivedNumber == 1) {
         open()
+        basic.pause(100)
     } else if (receivedNumber == 3) {
         close()
+        basic.pause(100)
     } else if (receivedNumber == 7) {
         軌道向左轉7()
+        basic.pause(100)
     } else if (receivedNumber == 9) {
         軌道向右轉9()
+        basic.pause(100)
     } else if (receivedNumber == 8) {
         軌道上升8()
+        basic.pause(100)
     } else if (receivedNumber == 2) {
         軌道下降2()
+        basic.pause(100)
     } else if (receivedNumber == 4) {
         向左跑4()
+        basic.pause(100)
     } else if (receivedNumber == 6) {
         向右跑6()
+        basic.pause(100)
+    } else {
+        停()
     }
 })
 function 軌道向右轉9 () {
     pwm = 255
     sensors.DDMmotor(
-    AnalogPin.P11,
+    AnalogPin.P2,
     1,
     AnalogPin.P12,
     pwm - 155
@@ -43,7 +53,7 @@ function 軌道下降2 () {
     AnalogPin.P15,
     1,
     AnalogPin.P16,
-    pwm - 155
+    pwm
     )
 }
 function 向左跑4 () {
@@ -73,13 +83,33 @@ function 軌道上升8 () {
     AnalogPin.P15,
     0,
     AnalogPin.P16,
-    pwm - 155
+    pwm
+    )
+}
+function 停 () {
+    sensors.DDMmotor(
+    AnalogPin.P11,
+    0,
+    AnalogPin.P12,
+    0
+    )
+    sensors.DDMmotor(
+    AnalogPin.P13,
+    0,
+    AnalogPin.P14,
+    0
+    )
+    sensors.DDMmotor(
+    AnalogPin.P15,
+    0,
+    AnalogPin.P16,
+    0
     )
 }
 function 軌道向左轉7 () {
     pwm = 255
     sensors.DDMmotor(
-    AnalogPin.P11,
+    AnalogPin.P2,
     0,
     AnalogPin.P12,
     pwm - 155
